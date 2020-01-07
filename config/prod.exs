d39c379c7e6c84c90b5f2aed6bf76c3a98862ced
Wasswa Samuel <latimerscope@gmail.com>
2020-01-07 17:20:38 +0200

    Force SSL

--- config/prod.exs
+++ config/prod.exs
@@ -52,4 +52,7 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
+config :patchtrace, PatchtraceWeb.Endpoint,
+   force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
 import_config "prod.secret.exs"
