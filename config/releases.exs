4615576c39cba812e86b6b5b7bb91d3b71f35feb
Wasswa Samuel <latimerscope@gmail.com>
2020-01-07 20:35:15 +0200

    Add custom domain to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,5 @@ import Config
 config :patchtrace, PatchtraceWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  #url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: "www.patchtrace.com", port: 443]
