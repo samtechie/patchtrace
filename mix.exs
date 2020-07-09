90782c640360b18254dddd3ea3729fe92900a75d
Wasswa Samuel <latimerscope@gmail.com>
2020-07-09 17:03:03 +0200

    Chore: Bump Live_view to 0.14

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Patchtrace.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_view, "~> 0.13.3"},
+      {:phoenix_live_view, "~> 0.14.0"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
